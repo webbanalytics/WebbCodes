0156ee230a0b7e54b19f111ce0a6f1a3cccbefa4
webbs3 <34485730+webbs3@users.noreply.github.com>
2018-07-05 17:13:04 -0400

    tweets as function

--- RunnerProfiles.R
+++ RunnerProfiles.R
@@ -15,39 +15,52 @@ elims <- maze_match_desc | blade_match_desc | blade_match_sn | maze_match_sn
 elimrunners<- runners[elims,]
 cleanrunners<- runners[!elims,]
 
-runlist <- runners$screen_name
-runlist2 <- cleanrunners$screen_name
+run_twdf <- timeline_gather(cleanrunners)
+run_twdf_new$sample <- 1
 
+#----------------------------------------------------------------------------
+#Find 'random' sample of other users
+others <- search_tweets(q="",n=1000, lang= "en", exclude_replies = TRUE, include_rts = FALSE)
+countdown()
+oth_twdf <- timeline_gather(others, interv = 180)
+oth_twdf$sample <- 0
 
+#-----------------------------------------------------------------------------
+#Combine and compare
+df <- rbind(run_twdf_new, oth_twdf)
+df$sample <- factor(df$sample, levels = c(0,1), labels = c("Non-Runners", "Runners"))
+
+#write.csv(df, file = "RunnerComparison.csv")
+
+#_________________________________________________________________________
+#Function to tell you have long before the next gathering of tweets
+# (because I'm impatient and need constant updates...)
 countdown <- function(minutes=15){
   for (i in 1:minutes){
     print(paste((minutes+1)-i, "minutes left..."))
     Sys.sleep(60)
   }}
 
-
+#__________________________________________________________________________
+timeline_gather <- function (df, interv = 180) {
+usersnames <- df$screen_name
 i=1
 j=1
-numrun=length(runlist2)
-interv=180-1
+numrun=length(usersnames)
+interv=interv-1
 twlist <- list()
-#countdown(15)
 #For each array of runners, download 10 most recent tweets then shift to next array
-while (j<numrun){
-if (j+interv<numrun){
-  twlist[[i]] <- get_timelines(user=runlist2[j:(j+interv)], n=10, exclude_replies = TRUE, include_rts = FALSE)
-  j = j+interv
-  i = i+1
-  countdown(15)
-} else {
-  twlist[[i]] <- get_timelines(user=runlist2[j:numrun], n=10, exclude_replies = TRUE, include_rts = FALSE)
-  j = numrun}
-}
-#Save as dataframe
-run_twdf_new <- do.call("rbind", twlist)
-
-
-
-
-
-#run_tweets <- get_timelines(user=runlist2, n=10, exclude_replies = TRUE, include_rts = FALSE)
+  while (j<numrun){
+    print(paste("Starting round", i, "..."))
+    if (j+interv<numrun){
+      twlist[[i]] <- get_timelines(user=usersnames[j:(j+interv)], n=10, exclude_replies = TRUE, include_rts = FALSE)
+      print(paste("Round", i, "finished at", Sys.time(), ". Pausing 15 mins..."))
+      Sys.sleep(60*15)
+      j = j+interv
+      i = i+1
+    } else {
+      twlist[[i]] <- get_timelines(user=usersnames[j:numrun], n=10, exclude_replies = TRUE, include_rts = FALSE)
+      j = numrun}
+  }
+return(do.call("rbind", twlist))}
+#__________________________________________________________________________
