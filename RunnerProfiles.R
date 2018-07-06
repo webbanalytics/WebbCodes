4ff97e57f94424bba32448f851100c5ee6817607
webbs3 <34485730+webbs3@users.noreply.github.com>
2018-07-06 17:26:14 -0400

    July 6 Changes

--- RunnerProfiles.R
+++ RunnerProfiles.R
@@ -1,6 +1,9 @@
 library(tidytext)
 library(rtweet)
 library(stringr)
+library(dplyr)
+library(ggplot2)
+library(wordcloud)
 
 runners <- search_users(q = 'runner' ,n=1000)
 
@@ -28,11 +31,81 @@ oth_twdf$sample <- 0
 #-----------------------------------------------------------------------------
 #Combine and compare
 df <- rbind(run_twdf_new, oth_twdf)
-df$sample <- factor(df$sample, levels = c(0,1), labels = c("Non-Runners", "Runners"))
+df$sample <- factor(df$sample, levels = c(0,1), labels = c("nonRunners", "Runners"))
+table(df$sample)
 
-write.csv(df[, c("text","screen_name", "sample")], file = "RunnerComparison.csv")
+df$timestamp <- as.POSIXct(df$created_at)
+min(df[(df$sample=="nonRunners"),]$timestamp)
 
-#_________________________________________________________________________
+
+df %>%
+  filter(timestamp > "2018-01-01") %>%
+  ggplot(aes(x=timestamp)) +
+    geom_freqpoly(colour="Blue") +
+    facet_grid(sample~.) +
+    xlab("Date") + ylab("Number of Tweets") +
+    theme_minimal()
+
+#geocode_apply<-function(x){
+  #geocode(x, source = "google", output = "all", api_key="xxx")
+#}
+  
+df$line <- 1:nrow(df)
+  
+replace_reg <- "https://t.co/[A-Za-z\\d]+|http://[A-Za-z\\d]+|&amp;|&lt;|&gt;|RT|https"
+unnest_reg <- "([^A-Za-z_\\d#@']|'(?![A-Za-z_\\d#@]))"
+tidy_tweets <- df %>% 
+  select(line, text, sample) %>%
+  filter(!str_detect(text, "^RT")) %>%
+  mutate(text = str_replace_all(text, replace_reg, "")) %>%
+  unnest_tokens(word, text, token = "regex", pattern = unnest_reg) %>%
+  filter(!word %in% stop_words$word,
+         str_detect(word, "[a-z]"))
+
+tidy_tweets %>%
+  filter(sample=="nonRunners") %>%
+  count(word) %>%
+  with(wordcloud(word, colors = 'navy', n, max.words = 100))
+
+tidy_tweets %>%
+  filter(sample=="Runners") %>%
+  count(word) %>%
+  with(wordcloud(word, colors = 'navy', n, max.words = 100))
+
+frequency <- tidy_tweets %>% 
+  group_by(sample) %>% 
+  count(word, sort = TRUE) %>% 
+  left_join(tidy_tweets %>% 
+              group_by(sample) %>% 
+              summarise(total = n())) %>%
+  mutate(freq = n/total)
+
+frequency
+
+library(tidyr)
+
+frequency <- frequency %>% 
+  select(sample, word, freq) %>% 
+  spread(sample, freq) %>%
+  arrange(nonRunners, Runners)
+
+frequency
+
+library(scales)
+
+ggplot(frequency, aes(nonRunners, Runners)) +
+  geom_jitter(alpha = 0.1, size = 2.5, width = 0.25, height = 0.25) +
+  geom_text(aes(label = word), check_overlap = TRUE, vjust = 1.5) +
+  scale_x_log10(labels = percent_format()) +
+  scale_y_log10(labels = percent_format()) +
+  geom_abline(color = "red")
+
+
+
+write.csv(df[, c("text","screen_name", "sample", "line", "timestamp")], file = "RunnerComparison.csv")
+
+
+#_____________________FUNCTIONS___________________________________________
 #Function to tell you have long before the next gathering of tweets
 # (because I'm impatient and need constant updates...)
 countdown <- function(minutes=15){
